c0908037bda9ff4396b78afb3922102a8514c6d1
Layne McNish <lmcnish14@gmail.com>
2014-10-19 16:58:55 -0600

    sorted answers in test and added benchmarking to program

--- lib/legitscript.rb
+++ lib/legitscript.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 class LegitScript
 
   def function(a)
@@ -11,7 +13,7 @@ class LegitScript
   def count_duplicates(input_array)
     items_count = Hash.new(0)
     input_array.each do |item|
-      items_count[item] += 1
+     items_count[item] += 1
     end
     items_count
   end
@@ -30,4 +32,11 @@ class LegitScript
 
 end
 
+a = (0..100).collect{ rand(1000000000000000) }
+b = (0..100).collect{ rand(1000000000000000) }
+c = a + b
 
+Benchmark.bmbm do |x|
+  x.report("function") { LegitScript.new.function(c) }
+  x.report("my method")  { LegitScript.new.return_duplicated_values(c)  }
+end
