9d1e0e4c22f29992a2418458d002c3ce515f2a46
Layne McNish <lmcnish14@gmail.com>
2014-10-19 18:19:02 -0600

    final benchmarking finished for graphing purposes

--- lib/legitscript.rb
+++ lib/legitscript.rb
@@ -43,9 +43,24 @@ class LegitScript
     end
     output.close
   end
+
+  def benchmark_duplicated_methods
+    output = File.open( "/Users/laynemcnish/gSchoolWork/Scratch/legitscript/bin/duplicate_benchmarks.txt","w" )
+    (1000..100000).step(1000).each do |i|
+      a = (0..i).collect { rand(10000) }
+      b = (0..i).collect { rand(10000) }
+      c = a + b
+      func_time = Benchmark.realtime do
+        LegitScript.new.return_duplicated_values(c)
+      end
+      output << "#{c.length}\t#{func_time}\n"
+    end
+    output.close
+  end
 end
 
 LegitScript.new.benchmark_function
+LegitScript.new.benchmark_duplicated_methods
 
 
 
